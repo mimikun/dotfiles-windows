157469aee33e54cb27000c7c12e32a9efed83db9
Yuto Tanaka <karaage_nennen@icloud.com>
2023-08-06 17:01:28 +0900

    Update scripts/update.ps1

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -1,3 +1,104 @@
-# update
-Write-Output "update.ps1 IS WIP"
+# Update
 
+chezmoi add C:\Users\dzdzb\AppData\Local\contour\contour.yml
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\Justfile
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\README.md
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\coc-settings.json
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\init.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lazy-lock.json
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\event_learn_support.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\keymaps.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\lazyvim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\options.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\alpha.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\calendar-vim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\chezmoi-vim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\coc-nvim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\copilot.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\dashboard-nvim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\denops-docker-vim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\dps-translate-vim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\fern.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\fzf.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\gin-vim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\glow-nvim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\jaq-nvim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\kdl.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\lualine.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\mastodon-nvim.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\noice.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\nvim-nu.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\nvim-surround.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\themes.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\todo-comments.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\tree-sitter-just.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\tree-sitter.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\trouble.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-cargo-make.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-gitgutter.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-just.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-markdown.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-quickrun.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-scouter.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vim-startuptime.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\vimdoc-ja.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\lua\plugins\which-key.lua
+chezmoi add C:\Users\dzdzb\AppData\Local\nvim\stylua.toml
+
+chezmoi add C:\Users\dzdzb\AppData\Roaming\CorvusSKK\userdict.txt
+chezmoi add "C:\Users\dzdzb\AppData\Roaming\GitHub CLI\config.yml"
+chezmoi add "C:\Users\dzdzb\AppData\Roaming\GitHub CLI\hosts.yml"
+chezmoi add C:\Users\dzdzb\AppData\Roaming\Hyper\config.json
+chezmoi add C:\Users\dzdzb\AppData\Roaming\Hyper\.hyper.js
+chezmoi add C:\Users\dzdzb\AppData\Roaming\SKKFEP\skkuser.txt
+chezmoi add C:\Users\dzdzb\AppData\Roaming\alacritty\alacritty.yml
+chezmoi add C:\Users\dzdzb\AppData\Roaming\lsd\config.yaml
+chezmoi add C:\Users\dzdzb\AppData\Roaming\pueue\pueue.yml
+
+chezmoi add C:\Users\dzdzb\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+#chezmoi add C:\Users\dzdzb\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+#chezmoi add C:\Users\dzdzb\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+#chezmoi add C:\Users\dzdzb\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+chezmoi add C:\Users\dzdzb\applications.yaml
+chezmoi add C:\Users\dzdzb\cargo_packages.txt
+
+chezmoi add C:\Users\dzdzb\.config\starship.toml
+chezmoi add C:\Users\dzdzb\.config\wezterm\wezterm.lua
+chezmoi add C:\Users\dzdzb\.config\whkdrc
+
+chezmoi add C:\Users\dzdzb\.gitignore_global
+
+chezmoi add C:\Users\dzdzb\.logseq\config\config.edn
+chezmoi add C:\Users\dzdzb\.logseq\config\plugins.edn
+chezmoi add C:\Users\dzdzb\.logseq\preferences.json
+chezmoi add C:\Users\dzdzb\.rustup\settings.toml
+chezmoi add C:\Users\dzdzb\.ssh\config
+
+chezmoi add C:\Users\dzdzb\komorebi.json
+chezmoi add C:\Users\dzdzb\macros.txt
+
+chezmoi add C:\Users\dzdzb\utilities\DailyTask.bat
+chezmoi add C:\Users\dzdzb\utilities\DailyTask.ps1
+chezmoi add C:\Users\dzdzb\utilities\GamingMode.bat
+chezmoi add C:\Users\dzdzb\utilities\Mining.bat
+chezmoi add C:\Users\dzdzb\utilities\MiningMode.bat
+chezmoi add C:\Users\dzdzb\utilities\Reboot.bat
+chezmoi add C:\Users\dzdzb\utilities\RebootUbuntu.bat
+chezmoi add C:\Users\dzdzb\utilities\Shutdown.bat
+chezmoi add C:\Users\dzdzb\utilities\StopTrex.bat
+chezmoi add C:\Users\dzdzb\utilities\StopTrex.ps1
+chezmoi add C:\Users\dzdzb\utilities\Ter.bat
+chezmoi add C:\Users\dzdzb\utilities\capslock2ctrl.reg
+chezmoi add C:\Users\dzdzb\utilities\dual-gminer-mining.bat
+chezmoi add C:\Users\dzdzb\utilities\gminer-flux-mining.bat
+chezmoi add C:\Users\dzdzb\utilities\gminer-mining.bat
+chezmoi add C:\Users\dzdzb\utilities\install_cargo_packages.ps1
+chezmoi add C:\Users\dzdzb\utilities\lolminer-mining.bat
+chezmoi add C:\Users\dzdzb\utilities\miniz-flux-mining.bat
+chezmoi add C:\Users\dzdzb\utilities\port_forwarding.ps1
+chezmoi add C:\Users\dzdzb\utilities\t-rex-mining.bat
+
+chezmoi add C:\Users\dzdzb\vimfiles\coc-settings.json
+chezmoi add C:\Users\dzdzb\vimfiles\vimrc
