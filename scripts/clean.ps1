7b2d51ab170fa251a8105e8d55dbdda60c32ba0d
Yuto Tanaka <karaage_nennen@icloud.com>
2023-08-07 20:22:12 +0900

    Update scripts/clean.ps1

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -2,3 +2,17 @@
 Remove-Item *.patch
 Remove-Item *.patch.gpg
 Remove-Item *.zip
+
+if ($env:COMPUTERNAME -eq "TANAKAPC") {
+    Remove-Item $env:USERPROFILE\utilities\GamingMode.bat
+    Remove-Item $env:USERPROFILE\utilities\Mining.bat
+    Remove-Item $env:USERPROFILE\utilities\MiningMode.bat
+    Remove-Item $env:USERPROFILE\utilities\StopTrex.bat
+    Remove-Item $env:USERPROFILE\utilities\StopTrex.ps1
+    Remove-Item $env:USERPROFILE\utilities\dual-gminer-mining.bat
+    Remove-Item $env:USERPROFILE\utilities\gminer-flux-mining.bat
+    Remove-Item $env:USERPROFILE\utilities\gminer-mining.bat
+    Remove-Item $env:USERPROFILE\utilities\lolminer-mining.bat
+    Remove-Item $env:USERPROFILE\utilities\miniz-flux-mining.bat
+    Remove-Item $env:USERPROFILE\utilities\t-rex-mining.bat
+}
